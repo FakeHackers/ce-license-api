ad21b6bf7443cca4e637d779864a0a0f789cdac8
FakeHackers <81543233+FakeHackers@users.noreply.github.com>
2025-12-06 18:33:04 +0700

    Refactors

--- public/admin/app.js
+++ public/admin/app.js
@@ -1,37 +1,27 @@
-function token() {
-  const t = document.getElementById("token").value;
-  if (!t) {
-    alert("Admin token required");
-    throw "NO_TOKEN";
-  }
-  return t;
-}
+const token = prompt("Admin Token");
 
-function post(url, data) {
+function call(url, data) {
   return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-admin-token": token()
+      "x-admin-token": token
     },
-    body: JSON.stringify(data)
-  }).then(r => r.json());
-}
-
-function show(res) {
-  document.getElementById("out").textContent =
-    JSON.stringify(res, null, 2);
+    body: JSON.stringify(data || {})
+  }).then(r => r.json()).then(alert);
 }
 
 function add() {
-  post("/admin/add", { key:key.value, days:days.value }).then(show);
+  call("/admin/add", {
+    key: key.value,
+    days: days.value
+  });
 }
-function extend() {
-  post("/admin/extend", { key:key.value, days:days.value }).then(show);
+
+function ban() {
+  call("/admin/ban", { key: key.value });
 }
-function ban(state) {
-  post("/admin/ban", { key:key.value, state }).then(show);
-}
-function del() {
-  post("/admin/delete", { key:key.value }).then(show);
+
+function backup() {
+  window.location = "/admin/backup";
 }
